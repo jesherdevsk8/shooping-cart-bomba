1ac861af2df0ab017eddd8b4c3271e107e0858f8
Jesher Minelli <jesherdevsk8@gmail.com>
2025-02-07 17:42:21 -0300

    improve procucts seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,9 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
 
-
-Product.create([
-  {
-    name: 'Samsung Galaxy S24 Ultra',
-    price: 12999.99
-  },
-  {
-    name: 'iPhone 15 Pro Max',
-    price: 14999.99
-  },
-  {
-    name: 'Xiamo Mi 27 Pro Plus Master Ultra',
-    price: 999.99
-  }
-])
+## create products
+5.times do
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price
+  )
+end
